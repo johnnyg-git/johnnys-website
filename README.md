276773a64f25706bc5bf7321e4cbbd0f0a706ac9
Joseph Goold <55810567+johnnyg-git@users.noreply.github.com>
2025-03-08 01:27:23 +0000

    Make the switch to using SvelteKit, as well as beginning to build the website itself. Currently, there is a working navigation bar, that automatically resizes itself if you scroll down, or if the screen is small. This navigation bar features custom buttons, with drop shadow and hover effects.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Johnny's Website
+
+![language svelte](https://img.shields.io/badge/language-svelte-orange)
+
+## About
+
+This project is made to act as a personal website for [jgoold.me](https://jgoold.me) / [johnnygjohnny.co.uk](https://johnnygjohnny.co.uk)
+
+## Installation
+
+1. **Clone the repository:**
+
+   ```bash
+   git clone https://github.com/johnnyg-git/johnnys-website.git
+   cd johnnys-website
+   ```
+
+2. **Install dependencies:**
+
+   ```bash
+   npm install
+   ```
+
+3. **Start the development server:**
+
+   ```bash
+   npm run dev
+   ```
+
+4. **Open your browser and visit:**
+   http://localhost:5173
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+
+	// Create a state for whether the header is expanded or not
+	let expanded = $state(false);
+	let resized = $state(false);
+	let y = 0;
+
+	import { onMount } from 'svelte';
+	onMount(() => {
+		const headernavbar = document.querySelector('.headernavbar');
+		if (!headernavbar) return;
+		y = 8;
+		window.addEventListener('scroll', () => {
+			expanded = window.scrollY > y || resized;
+		});
+
+		window.addEventListener('resize', () => {
+			resized = window.innerWidth < 1000;
+			expanded = window.scrollY > y || resized;
+		});
+	});
+</script>
+
+<div class="headernavbar {expanded ? 'expanded' : ''} {resized ? 'resized' : ''}">
+	<a href="/">
+		<img src="/logo.png" class="navlogo hovershadow" alt="logo" width="50" height="50" />
+	</a>
+	<nav>
+		<a href="/" class="navbutton hovershadow">Home</a>
+		<a href="/about" class="navbutton hovershadow">About</a>
+		<a href="/thisdoesntexist" class="navbutton hovershadow">Super Secret</a>
+	</nav>
+</div>
+
+{@render children()}
+
+<style>
+	.headernavbar {
+		box-sizing: border-box;
+		background-color: #282828;
+		color: #fff;
+		border-radius: 15px;
+		display: flex;
+		align-items: center;
+		justify-content: center; /* Center children horizontally */
+		padding: 0.5em 1em;
+		position: sticky;
+		top: 0;
+		margin: 0 auto;
+		margin-top: 8px;
+		max-width: 400px;
+		border-bottom: 3px solid #7b3866;
+		will-change: max-width, border-bottom, margin-top;
+		transition:
+			max-width 0.3s ease,
+			border-bottom 0.3s ease,
+			margin-top 0.3s ease;
+		z-index: 1000;
+	}
+
+	.headernavbar.expanded {
+		max-width: 100%; /* Full width when expanded */
+		border-radius: 0;
+	}
+
+	.headernavbar.resized {
+		margin-top: 0;
+	}
+
+	.navlogo {
+		margin-right: 1em;
+		border-radius: 50%;
+	}
+
+	.navbutton {
+		display: inline-flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+		background-color: transparent;
+		border: none;
+		color: inherit;
+		text-decoration: none;
+		font-size: 1em;
+		border-radius: 5px;
+		cursor: pointer;
+		padding: 1em 1em;
+
+		will-change: background-color;
+		transition: background-color 0.3s ease;
+	}
+
+	.navbutton:hover {
+		background-color: #7b3866;
+	}
+
+	.hovershadow {
+		will-change: filter;
+		transition: filter 0.3s ease;
+	}
+
+	.hovershadow:hover {
+		filter: drop-shadow(0 0 0.5rem #7b3866);
+	}
+</style>
