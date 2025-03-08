627a7d1410f56881f3b0da84a1c9f9c993c66bb3
Joseph Goold <55810567+johnnyg-git@users.noreply.github.com>
2025-03-08 14:46:35 +0000

    Add a basic "Under Construction" message on the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,26 @@
 	<title>Home</title>
 </svelte:head>
 
-<h1>Hello</h1>
+<div class="container">
+	<div class="message">
+		<p>This website is currently under construction...</p>
+	</div>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+	.message {
+		text-align: center;
+		background-color: #282828;
+		font-size: 1.5rem;
+		border-radius: 25px;
+		padding: 20px;
+
+		filter: drop-shadow(0 0 0.5rem #7b3866);
+	}
+</style>
