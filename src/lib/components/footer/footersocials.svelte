3a03fe2e9cb17266ed7d34d6a11fe7d1463d5e28
Joseph Goold <55810567+johnnyg-git@users.noreply.github.com>
2025-03-11 21:29:32 +0000

    Simplify the styling for the footer socials, there is no need to be using a whole separate  class. Add padding to the icons so that they are easier to select

--- src/lib/components/footer/footersocials.svelte
+++ src/lib/components/footer/footersocials.svelte
@@ -4,39 +4,29 @@
 </script>
 
 <div>
-	<a
-		href="https://github.com/johnnyg-git"
-		target="_blank"
-		rel="noopener noreferrer"
-		class="footersocial"
-	>
+	<a href="https://github.com/johnnyg-git" target="_blank" rel="noopener noreferrer">
 		<IconMdiGithub />
 	</a>
-	<a href="mailto:inquiries@johnnygjohnny.co.uk" class="footersocial">
+	<a href="mailto:inquiries@johnnygjohnny.co.uk">
 		<IconMdiEmail />
 	</a>
 </div>
 
 <style>
-	.footersocial {
-		display: inline-block;
-		margin: 0 0.5em;
-		transition: transform 0.3s ease;
-	}
-
-	.footersocial:hover {
-		transform: translateY(-5px);
-	}
-
 	a {
+		display: inline-block;
+		padding: 10px;
 		color: var(--primary-color);
 		text-decoration: none;
 		font-weight: normal;
-		transition: color 0.3s ease;
-		will-change: color;
+		transition:
+			color 0.3s ease,
+			transform 0.3s ease;
+		will-change: color, transform;
 	}
 
 	a:hover {
 		color: var(--accent-color);
+		transform: translateY(-5px);
 	}
 </style>
