982b6f270ef42071efa9cd1d3118c64b0b64b5b3
Joseph Goold <55810567+johnnyg-git@users.noreply.github.com>
2025-03-11 20:27:19 +0000

    Add temporary test sections to make sure smooth scrolling with navigation bar still works

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,44 @@
 <svelte:head>
 	<title>Johnny's Website</title>
 </svelte:head>
+
+<div class="content">
+	<div id="aboutSection" class="testSection">
+		<h2>About Section</h2>
+	</div>
+
+	<div id="contactSection" class="testSection">
+		<h2>Contact Section</h2>
+	</div>
+
+	<div id="catSection" class="testSection">
+		<h2>Cat Section</h2>
+	</div>
+</div>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		width: 100%;
+	}
+
+	.testSection {
+		height: 400px;
+		width: 500px;
+		margin: 20px 0;
+		padding: 20px;
+		border-radius: 8px;
+	}
+
+	#aboutSection {
+		background-color: var(--bg-color-secondary);
+	}
+	#contactSection {
+		background-color: var(--bg-color-secondary);
+	}
+	#catSection {
+		background-color: var(--bg-color-secondary);
+	}
+</style>
